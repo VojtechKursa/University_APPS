5b2c2cea719d6dd082de8d316303cea0eb17aff6
kur0170 <kur0170@pcfeib430-022-201.vsb.cz>
2022-02-15 11:50:26 +0100

    Finish Lab 2

--- Code/Lab_2/Lab_2/asm-module.asm
+++ Code/Lab_2/Lab_2/asm-module.asm
@@ -11,10 +11,96 @@
 
     section .data
     
-    
+    extern g_long, g_longsum, g_long2, g_long2sum
+    extern g_int_arr, g_int_index_min, g_evensum
 
 ;***************************************************************************
 
     section .text
     
-    
+
+    global sum_long
+sum_long:
+	mov al, 0
+	mov rcx, 0
+
+.begin:
+	cmp rcx, 8
+	jae .end
+
+	add al, [g_long + rcx]
+
+	inc rcx
+	jmp .begin
+.end:
+
+	mov [g_longsum], al
+	ret
+
+
+    global sum_long2
+sum_long2:
+	mov eax, 0
+	mov rcx, 0
+	mov edx, 0
+
+.begin:
+	cmp rcx, 8
+	jae .end
+
+	mov dl, [g_long2 + rcx]
+	add eax, edx
+
+	inc rcx
+	jmp .begin
+.end:
+
+	mov [g_long2sum], eax
+	ret
+
+
+	global find_min
+find_min:
+	mov eax, [g_int_arr]	;min
+	mov ecx, 1				;counter
+	mov r9d, 0				;min_index
+
+.begin:
+	cmp rcx, 10
+	jae .end
+
+	cmp [g_int_arr + ecx * 4], eax
+	jge .skip
+	mov eax, [g_int_arr + ecx * 4]
+	mov r9d, ecx
+
+.skip:
+	inc rcx
+	jmp .begin
+.end:
+
+	mov [g_int_index_min], r9d
+	ret
+
+
+	global sum_even
+sum_even:
+	mov eax, 0	;sum
+	mov ecx, 0	;counter
+	mov r9d, 1	;mask
+
+.begin:
+	cmp ecx, 10
+	jae .end
+
+	test r9d, [g_int_arr + 4*ecx]
+	jne .skip	;jnz
+	add eax, [g_int_arr + 4*ecx]
+
+.skip:
+	inc ecx
+	jmp .begin
+.end:
+
+	mov [g_evensum], eax
+	ret
--- Code/Lab_2/Lab_2/c-main.c
+++ Code/Lab_2/Lab_2/c-main.c
@@ -9,7 +9,34 @@
 
 #include <stdio.h>
 
+long g_long = 0x0807060504030201;
+unsigned char g_longsum;
+void sum_long();
+
+long g_long2 = 0x4040404040404040;
+int g_long2sum;
+void sum_long2();
+
+int g_int_arr[] = {100, -100, 205, -250, 1001, 8026, -7500, 903, 805, -1007};
+int g_int_index_min;
+void find_min();
+
+int g_evensum;
+void sum_even();
+
 int main()
 {
+	sum_long();
+	printf("g_longsum = %d\n", g_longsum);
+
+	sum_long2();
+	printf("g_long2sum = %d\n", g_long2sum);
+
+	find_min();
+	printf("g_int_index_min = %d\n", g_int_index_min);
+
+	sum_even();
+	printf("g_evensum = %d\n", g_evensum);
+
 	return 0;
 }
