96aeeabe382401eb74dc504bd486cd3d9848f6b1
kur0170 <kur0170@pcfeib430-022-201.vsb.cz>
2022-02-22 11:29:46 +0100

    Finish Lab 3

--- Code/Lab_3/Lab_3/asm-module.asm
+++ Code/Lab_3/Lab_3/asm-module.asm
@@ -11,10 +11,111 @@
 
     section .data
     
-    
-
 ;***************************************************************************
 
     section .text
     
-    
+    global search_amax
+;	RDI = arr, RSI = len
+search_amax:
+	mov rax, [rdi]	; out, curr_max
+	mov rcx, 1		; loop counter
+
+.begin:
+	cmp rcx, rsi
+	jge .end
+
+	mov r10, [rdi + 8 * rcx]
+	cmp r10, 0
+	jge .skip1
+	neg r10
+
+.skip1:
+	cmp r10, rax
+	jle .skip2
+	mov rax, r10
+
+.skip2:
+	inc rcx
+	jmp .begin
+.end:
+
+	ret
+
+
+	global search_mask
+;	RDI = arr, RSI = len, RDX = bits
+search_mask:
+	mov eax, 0	; out
+
+	mov rcx, 32
+	sub rcx, rdx
+	mov r10d, 0xFFFFFFFF	; mask
+	shr r10d, cl
+
+	mov rcx, 0	; loop counter
+
+.begin:
+	cmp rcx, rsi
+	jge .end
+
+	test [rdi + 4*rcx], r10d
+	jne .skip
+	inc eax
+
+.skip:
+	inc rcx
+	jmp .begin
+.end:
+
+	ret
+
+
+	global copy_nospace
+;	RDI = str_in, RSI = str_out
+copy_nospace:
+	mov rcx, 0		; loop counter
+	mov dl, 0		; buffer
+	mov r11, 0		; out counter
+
+.begin:
+	cmp byte [rdi + rcx], 0
+	je .end
+
+	cmp byte [rdi + rcx], ' '
+	je .skip
+	mov dl, [rdi + rcx]
+	mov [rsi + r11], dl
+	inc r11
+
+.skip:
+	inc rcx
+	jmp .begin
+.end:
+
+	mov byte [rsi + r11], 0
+	ret
+
+
+	global num_digits
+;	RDI = str_in
+num_digits:
+	mov rax, 0		; out
+	mov rcx, 0		; loop counter
+
+.begin:
+	cmp byte [rdi + rcx], 0
+	je .end
+
+	cmp byte [rdi + rcx], '0'
+	jb .skip
+	cmp byte [rdi + rcx], '9'
+	ja .skip
+	inc rax
+
+.skip:
+	inc rcx
+	jmp .begin
+.end:
+
+	ret
--- Code/Lab_3/Lab_3/c-main.c
+++ Code/Lab_3/Lab_3/c-main.c
@@ -9,7 +9,25 @@
 
 #include <stdio.h>
 
+long search_amax( long *t_array, int t_len );
+int search_mask( int *t_array, int t_len, int t_bits );
+void copy_nospace( const char *t_str_in, char *t_str_out );
+long num_digits( const char *t_str );
+
 int main()
 {
+	long long_arr[] = {5, -8, 500000, -800000, 7};
+	printf("%ld\n\n", search_amax(long_arr, 5));
+
+	int int_arr[] = {0x5, 0x4, 0x8, 0x80, 0xF8, 0xF1};
+	printf("%d\n\n", search_mask(int_arr, 6, 3));
+
+	const char str[] = " ab c  dx o59 .l 8x 5 p3a";
+	char newstr[50];
+	copy_nospace(str, newstr);
+	printf("%s\n\n", newstr);
+
+	printf("%ld\n\n", num_digits(str));
+
 	return 0;
 }
