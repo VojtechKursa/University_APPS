e29e3597b08853a12c850be112d3f75c59b98638
kur0170 <kur0170@pcfeib430-022-201.vsb.cz>
2022-03-01 11:56:10 +0100

    Finish Lab 4

--- Code/Lab_4/Lab_4/asm-module.asm
+++ Code/Lab_4/Lab_4/asm-module.asm
@@ -16,5 +16,153 @@
 ;***************************************************************************
 
     section .text
-    
-    
+
+
+	global even_nibbles
+;RDI = arr, RSI = n
+even_nibbles:
+	mov eax, 0
+	mov r8, 0	; loop counter
+	mov r9d, 2
+.1_begin:
+	cmp r8, rsi
+	jge .1_end
+
+	mov ecx, [rdi + r8 * 4]
+.2_begin:
+	cmp ecx, 0
+	je .return_b
+
+	jmp .return_e
+.return_b:
+	mov eax, [rdi + r8 * 4]
+	ret
+.return_e:
+
+	mov eax, 0xF
+	and eax, ecx
+
+	cdq
+	idiv r9d
+
+	cmp edx, 0
+	jne .2_end
+
+	shr ecx, 4
+	jmp .2_begin
+.2_end:
+
+	inc r8
+	jmp .1_begin
+.1_end:
+
+	mov eax, -1
+	ret
+
+
+    global under_avg
+;RDI = arr, RSI = n
+under_avg:
+	mov rax, 0
+	mov rcx, 0	; loop counter
+.1_begin:
+	cmp rcx, rsi
+	jge .1_end
+
+	add rax, [rdi + rcx * 8]
+
+	inc rcx
+	jmp .1_begin
+.1_end:
+
+	cqo
+	idiv rsi
+
+	mov r8, rax
+	mov rcx, 0
+	mov rax, 0
+.2_begin:
+	cmp rcx, rsi
+	jge .2_end
+
+	cmp [rdi + rcx * 8], r8
+	jge .2_skip
+	inc eax
+.2_skip:
+
+	inc rcx
+	jmp .2_begin
+.2_end:
+	ret
+
+
+    global most_common
+;RDI = arr, RSI = n
+most_common:
+	mov eax, 0		; most common num
+	mov edx, 0		; most common count
+	mov r11d, 50	; curr searched number
+.1_begin:
+	cmp r11d, 500
+	jg .1_end
+
+	mov rcx, 0	; loop counter
+	mov r10d, 0	; curr count
+
+.2_begin:
+	cmp rcx, rsi
+	jge .2_end
+
+	cmp r11d, [rdi + 4 * rcx]
+	jne .2_skip1
+	inc r10d
+.2_skip1:
+
+	inc rcx
+	jmp .2_begin
+.2_end:
+
+	cmp r10d, edx
+	cmova edx, r10d
+	cmova eax, r11d
+
+	add r11d, 50
+	jmp .1_begin
+.1_end:
+	ret
+
+
+	global shorter_vector
+;RDI = v1, RSI = v2
+shorter_vector:
+	mov rcx, 0
+	mov r8, 0	; v1 len
+	mov r9, 0	; v2 len
+
+.begin:
+	cmp rcx, 3
+	jge .end
+
+	mov eax, [rdi + 4 * rcx]
+	imul eax
+	shl rdx, 32
+	or rax, rdx
+	add r8, rax
+
+	mov eax, [rsi + 4 * rcx]
+	imul eax
+	shl rdx, 32
+	or rax, rdx
+	add r9, rax
+
+	inc rcx
+	jmp .begin
+.end:
+
+	cmp r8, r9
+	jle .ret_1
+	mov eax, 2
+	ret
+.ret_1:
+	mov eax, 1
+	ret
--- Code/Lab_4/Lab_4/c-main.c
+++ Code/Lab_4/Lab_4/c-main.c
@@ -9,7 +9,28 @@
 
 #include <stdio.h>
 
+int even_nibbles(int* arr, int n);
+int under_avg(long* arr, int n);
+int most_common(int* arr, int n);
+int shorter_vector(int* v1, int* v2);
+
 int main()
 {
+	//	1.
+	int arr_int[] = {0x59, 0x70, 0x1, 0x80, 0xFF, 50, 50, 100, 150, 300, 350, 350, 350, 400, 450, 450, 450, 500, 1000, 600, 450};
+	printf("%d\n\n", even_nibbles(arr_int, 21));	//0x80 = 128
+
+	//	2.
+	long arr_long[] = {500, 200, 1, 9, 600, 593, 250};
+	printf("%d\n\n", under_avg(arr_long, 7));	//4
+
+	//	3.
+	printf("%d\n\n", most_common(arr_int, 21));		//450
+
+	//	4.
+	int v1[] = {2,1,3};
+	int v2[] = {2,1,2};
+	printf("%d\n\n", shorter_vector(v1, v2));	//v2
+
 	return 0;
 }
